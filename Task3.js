9e99ef17a054de0027f75af9d90e7ba85195c579
TikhonovDaniil <62997681+TikhonovDaniil@users.noreply.github.com>
2024-05-27 13:35:56 +0300

    Add files via upload

---
+++ Task3.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+describe('Получение факта о котах', () => {
+
+  it('Отправка GET запроса и проверка параметров овета', async () => {
+    const response = await axios.get(`https://catfact.ninja/fact`);
+
+    expect(response.status).toBe(200)
+    expect(response.statusText).toBe("OK")
+    expect(response.data.fact).not.toBeNull()
+    expect(response.data.length).toBe(response.data.fact.length)
+    
+  });
+});
